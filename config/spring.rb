7edfe84c46e9fcc1a69b9b7a453e743f00a7771e
Evgeny Mironenko <evgenii@grabcad.com>
2019-04-04 17:59:43 +0300

    Initial commit

---
+++ config/spring.rb
@@ -0,0 +1,13 @@
+%w(
+  .ruby-version
+  .rbenv-vars
+  tmp/restart.txt
+  tmp/caching-dev.txt
+).each { |path| Spring.watch(path) }
+
+Spring.after_fork do
+  if ENV['DEBUGGER_STORED_RUBYLIB']
+    starter = ENV['BUNDLER_ORIG_RUBYOPT'][2..-1]
+    load(starter + '.rb')
+  end
+end
