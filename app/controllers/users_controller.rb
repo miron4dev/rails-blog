623415194f986769a7145b7294d27f218530964f
Evgeny Mironenko <evgenii@grabcad.com>
2019-04-10 18:01:02 +0300

    Some TODO's have been added as the reminders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,16 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    #  TODO allow only for the anonymous user
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "Welcome to the blog #{@user.username}" }
-        format.json { render :show, status: :created, location: @user }
+        format.html {redirect_to @user, notice: "Welcome to the blog #{@user.username}"}
+        format.json {render :show, status: :created, location: @user}
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @user.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -40,13 +41,14 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    #  TODO allow only for the current user
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html {redirect_to @user, notice: 'User was successfully updated.'}
+        format.json {render :show, status: :ok, location: @user}
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @user.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -54,21 +56,24 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    #  TODO allow only for the current user
+    #  TODO check that all the related articles are also destroyed
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to users_url, notice: 'User was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:email, :username, :password)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
 end
