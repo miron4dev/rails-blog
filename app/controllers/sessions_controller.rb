2ec97b20540b012ade90504d43f2ef62645dc3a1
Evgeny Mironenko <evgenii@grabcad.com>
2019-04-11 17:14:13 +0300

    Login form has been implemented

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    respond_to do |format|
+      if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        format.html {redirect_to root_path, notice: 'User was successfully authenticated.'}
+      else
+        flash[:alert] = 'Email / Password combination is invalid'
+        format.html {render :new}
+      end
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out'
+    redirect_to root_path
+  end
+end
