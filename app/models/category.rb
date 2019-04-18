5d1e65aaa33be81455e812ebdc213b987388d30f
Evgeny Mironenko <evgenii@grabcad.com>
2019-04-18 11:20:47 +0300

    Articles <-> Categories relationship has been implemented

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
 
+  has_many :article_categories, dependent: :delete_all
+  has_many :articles, through: :article_categories
+
   validates :name, uniqueness: true, presence: true, length: {minimum: 3, maximum: 25}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190416114032) do
+ActiveRecord::Schema.define(version: 20190417104803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id",  null: false
+    t.integer "category_id", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       null: false
